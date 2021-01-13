c74e116b6e88e3112485a25d78b0d18b4dd0ca46
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-01-13 11:53:45 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+            // const sis = () => 'моя функцыя';
+
+            // console.log(sis())
+
+
+
+            // const pep = ()  => 'evo67' 
+            // console.log(pep())
+
+
+
+            // const close = () => 'У нас закрыт магазин'
+            // console.log(close())
