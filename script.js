030335f10c60e06e74a3f5d65c7de0ac81cdb85b
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-01-15 09:32:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@
 
             // const close = () => 'У нас закрыт магазин'
             // console.log(close())
+
+
+   const newspapaer = Object.assign ({
+                time: '12-mp',
+                auto: 'bmw',
+                name: 'ilysha',
+            })
+
+            const newee = Object.assign  ({}, newspapaer ,{
+                emeil: "@mail.ru"
+            })
+            
+            
+                console.log(newee)
