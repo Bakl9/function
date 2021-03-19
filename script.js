1d27ff68b7d7e6e1eb105c0535873b5b0a2842d4
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-19 14:32:40 +0300

    Update script.js
    calculator

--- script.js
+++ script.js
@@ -86,4 +86,25 @@
     alert(hobby[key])
     let userName = prompt('Кто читает?')
     alert(programer[key])
+
+// function calculator 
+let calculator = {
+    sum () {
+        return this.a + this.b;
+    },
+
+    mul () {
+        return this.a * this.b;
+    },
+
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+};
+
+
+calculator.read();
+alert (calculator.sum());
+alert (calculator.mul());
  
